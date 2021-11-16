ee2d21caa1b5ad66549a7ac649b46a159f51b432
jaredtrp <jaredtrp@gmail.com>
2021-11-15 20:43:40 -0600

    added player and enemy health checks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,46 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10 + 10);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+  
+    enemyHealth = enemyHealth - playerAttack;
+    
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+    
+    playerHealth = playerHealth - enemyAttack;
+    
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+   
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      );
+      
+      // check enemy's health
+      if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+      } 
+      else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      }
 
-console.log("Our robot's name is " + playerName);
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+  };
 
-  // fight();
+fight();
